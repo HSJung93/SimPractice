74e608448bc69f22ba48fa5c1d4fa5ddf9574583
Hoesung Jung <jrps2212@gmail.com>
2020-09-25 17:15:40 +0900

    the remark for the code gsynth and panelmatch is updated

--- GSynth.r
+++ GSynth.r
@@ -1,57 +1,66 @@
 # Panel Data Simulation for GSynth
 ### https://www.cambridge.org/core/journals/political-analysis/article/generalized-synthetic-control-method-causal-inference-with-interactive-fixed-effects-models/B63A8BD7C239DD4141C67DA10CD0E4F3
 
+# Setting params for panel and treatment informations
+### 50 Units and 30 Time periods panel data will be simulated. 5 Units out of 50 units and 10 out of 30 time periods are treated. 
 N = 50
 N_tr = 5
 T = 30
 T_0= 20
-w = .8
 
+# Making fixed effects and treatment variables
+### time fixed effects are made up of normal dist. Actually, normal dist. is used when simulating random effects. 
 (Units.factor = rep(as.character(1:N) , T))
 (Units = rep(runif(N, -sqrt(3), sqrt(3)), T))
 (Times.factor = rep(1:T, each=N))
 (Times = rep(rnorm(T, 0, 1), each = N) )
-
 (Treat.factor = c( rep("control", N*T_0) , rep(c(rep("treatment", N_tr) , rep("control", (N - N_tr))), (T-T_0))))
 
-(lambda_i1 = runif(1, -sqrt(3), sqrt(3)) )
-(lambda_i2 = runif(1, -sqrt(3), sqrt(3)) )
+###(lambda_i1 = runif(1, -sqrt(3), sqrt(3)) ) for check
+###(lambda_i2 = runif(1, -sqrt(3), sqrt(3)) )
 
+# Making preliminary dataframe with basic factor variables
 dat1 = data.frame(Units.factor, Units, Times.factor, Times, Treat.factor)
 
+# Let's add the unit fixed effect for the treated with uniform distribution as the way illustrated in the paper
+dat1$Units[dat1$Treat.factor == "treatment"] = runif(1, sqrt(3) - 2*w*sqrt(3) , 3*sqrt(3) - 2*w*sqrt(3) )
+
+# Making two factors and two factor loadings
+## factors are made of uniform distribution with weight w = .8. it represent uncertainty difference between treated and control data 
 w = .8
 dat1$lambda_i1 = rep(0, N*T)
 dat1$lambda_i1 = rep(runif(N, sqrt(3) - 2*w*sqrt(3) , 3*sqrt(3) - 2*w*sqrt(3) ), T)
 dat1$lambda_i1[dat1$Treat.factor == "treatment"] = rep(runif(N_tr, -sqrt(3), -sqrt(3)), T-T_0)
 
-dat1$Units[dat1$Treat.factor == "treatment"] = runif(1, sqrt(3) - 2*w*sqrt(3) , 3*sqrt(3) - 2*w*sqrt(3) )
-
 dat1$lambda_i2 = rep(0, N*T)
 dat1$lambda_i2 = rep(runif(N, sqrt(3) - 2*w*sqrt(3) , 3*sqrt(3) - 2*w*sqrt(3) ), T)
 dat1$lambda_i2[dat1$Treat.factor == "treatment"] = rep(runif(N_tr, -sqrt(3), -sqrt(3)), T-T_0)
 
+## make error term with normal dist. for simulating treatment effects
 e = rnorm(N*(T-T_0), 0, 1)
 
+## make treatment effect delta. treatment starts at period 20
 dat1$delta = rep(0, N*T)
 dat1$delta = c( rep(0, N*T_0) , rep(1:10, each = N) + e )
 dat1$delta
 
+## factors are made up of normal distribution
 dat1$f_1t = rep(rnorm(N, 0, 1), each = T)
 dat1$f_2t = rep(rnorm(N, 0, 1), each = T)
 
+# Simulate X from factor and factor loadings
+## setting params and errors for simulating x variables with the other varaibles
 b1 = 1
 b2 = 3
-
 eta1 = rnorm(N*T, 0, 1)
 eta2 = rnorm(N*T, 0, 1)
 
 dat1$x1 = with(dat1, 1 + lambda_i1*f_1t + lambda_i2*f_2t + lambda_i1 + lambda_i2 + f_1t + f_2t + eta1 )
 dat1$x2 = with(dat1, 1 + lambda_i1*f_1t + lambda_i2*f_2t + lambda_i1 + lambda_i2 + f_1t + f_2t + eta2 )
 
+# Simulate Y from error e and all the other variables generated previously. 
 eps = rnorm(N*T, 0, 1)
-
 dat1$Y = with(dat1, delta*(Treat.factor == "treatment") + x1*b1 + x2*b2 + lambda_i1*f_1t + lambda_i2*f_2t + Units + Times + 5 + eps )
 
-dat1$Y
-
+# Plot x and Y variables
 plot(dat1$x1, dat1$Y)
--- PanelMatch.r
+++ PanelMatch.r
@@ -4,67 +4,106 @@
 
 set.seed(20200920)
 
+# Making Z and Fixed Effect Variables
+## setting params for basic variables
 N = 162
 T = 51
 Z_bin_prob = .5
 
+## simulating r.v
 (X_zero = rep("control", n = N*T))
+
+## unit fixed effect
 (Units = rep(runif(N, 0, 1),T))
 (Units.factor = rep(as.character(1:N) , T))
+
+## time fixed effect
+### time factor is distirbuted with """EACH""" value of the seq(1:T). it matters when making x variables from previous z and fixed effect variables
 (Times = rep(runif(T, 0, 1), each = N))
 (Times.factor = rep(as.character(1:T), each=N))
+
+## covariate conti. bin.
 (Z_conti = runif(n = N*T, min=0, max=1))
 (Z_bin = rbinom(n = N*T, size = 1, prob = Z_bin_prob))
 
+## function for making lag variables
 lag_vector = function(vector){
     lag_output = c(0, vector[1:(length(vector)-1)])
     lag_output
 }
 
+## lag variables
 Z_conti.lag = lag_vector(Z_conti)
 Z_bin.lag = lag_vector(Z_bin)
 Z_conti.lag2 = lag_vector(Z_conti.lag)
 Z_bin.lag2 = lag_vector(Z_bin.lag)
 
+# Simulating X Varibles
+## zero vector for simulating x variables
+### we will make x variables from previous z and fixed effect variables
 X = rep(0, N*T) 
 X.lag = rep(0, N*T)
 X.lag2 = rep(0, N*T)
 
+## make dataframe from variables above
 dat.stage1 = data.frame(Units, Units.factor, Times, Times.factor, Z_conti, Z_bin, Z_conti.lag, Z_bin.lag, Z_conti.lag2, Z_bin.lag2, X, X.lag, X.lag2)
 
+## setting params for making X variables form Z and Fixed effect variables.
+### coefficient of z conti.
 ksi.zc = -.06
+
+### coeff. of z bin.
 psi.zb = .03
+
+### coeff. of time correlation 
 rho = .01
+
+### coeff. of x
 beta.x = - 0.75
 
+## make x variables from previous z and fixed effect variables
 treatment_generator = function(dat.stage1, t ){
 
     for (i in 1:t){
+
+        ### setting params for making x variables from previous z and fixed effect variables. 
+        ### because time factor is distributed with N times EACH values of seq 1:T, we can make x variables from  
         start = (t-1)*N + 1
         end = t*N 
 
+        ### because X variables are made of zero vectors temporally, whe can use lag variables of X even in first data gernerating stage.
         lagged = with(dat.stage1[start:end, ], Units + Times + ksi.zc*Z_conti + psi.zb*Z_bin + rho*ksi.zc*Z_conti.lag + rho*psi.zb*Z_bin.lag + rho^2*ksi.zc*Z_conti.lag2 + rho^2*psi.zb*Z_bin.lag2 + beta.x*X.lag + rho*beta.x*X.lag2 )
 
+        ### X which is a vector of binary treatment values can be generated with inverse logit fucntion and binomial fucntion 
         inverse.logit = plogis(lagged)
-
         dat.stage1[start:end, ]$X = rbinom(n = N, size = 1, prob = inverse.logit)
+        
         dat.stage1[(start+N):(end+N),]$X.lag = dat.stage1[start:end, ]$X
-        dat.stage1[(start+2*N):(end+2*N),]$X.lag2 = dat.stage1[start:end, ]$X
+        dat.stage1[(start+2*N):(end+2*N),]$X.lag2 = dat.stage1[start:end, ]$X # X's values of period t is the same with X.lags2's values of (t+2N) period after.
     }
 
+    ### the output will be the dataframe with generated X variables
     dat.stage1
 }
 
 dat.output = treatment_generator(dat.stage1, T)
+
+# Simulating Y Variables
+## subsetting dataframe
+### the length of the dat.output exceeds the length of the input dataframe because of the lag terms, so we make new dataframe dat.y with right length.
 dat.y = dat.output[1:(N*T), ]
 
-$eps = rnorm(N*T, 0, 1)
+## making epsilon for simulating Y from the other varibles
+dat.y$eps = rnorm(N*T, 0, 1)
 
+## setting params for simulating Y form X and Z and Fixed effects terms.
 ksi.zc.y = -.6
 psi.zb.y = .3
 rho.y = .4
 beta.y = -7.5
 
-dat.y$Y = with(dat.y, Units + Times + ksi.zc.y*Z_conti + psi.zb.y*Z_bin + rho.y*ksi.zc.y*Z_conti.lag + rho.y*psi.zb.y*Z_bin.lag + rho.y^2*ksi.zc.y*Z_conti.lag2 + rho^2*psi.zb.y*Z_bin.lag2 + beta.y*X + rho.y*beta.y*X.lag + rho.y^2*beta.y*X.lag2 )
+dat.y$Y = with(dat.y, Units + Times + ksi.zc.y*Z_conti + psi.zb.y*Z_bin + rho.y*ksi.zc.y*Z_conti.lag + rho.y*psi.zb.y*Z_bin.lag + rho.y^2*ksi.zc.y*Z_conti.lag2 + rho^2*psi.zb.y*Z_bin.lag2 + beta.y*X + rho.y*beta.y*X.lag + rho.y^2*beta.y*X.lag2 + eps)
+### eps was omitted in previous code. eps was added at 20200924
 
+# Plot the treatment variable and the dependant variable.
 plot(dat.y$X, dat.y$Y)
